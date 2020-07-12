fc51e9526f22ba57278a8edfcd88af92bc1e8736
shreyash <sai.itankar00001@gmail.com>
2020-07-12 20:38:22 +0530

    made some changes to code

--- assignment.py
+++ assignment.py
@@ -4,13 +4,16 @@ class WrongValueError(Exception):
        user wrong entered value'''
     pass
 
-class Gst:
-    '''
+'''
+    @author
+    @method
+    @description
     It is an application which takes input 
     for the product quality, unit, name, price
     and provide calculated final price including 
     the GST of the perticular product category.
     '''
+class Gst:
     __final_price=0
     __slab_code=0  
     __gst_applicable = 0
@@ -27,10 +30,12 @@ class Gst:
                                 703:'electronics',
                                 704: 'cosmetics'}
                      
-    #Method to set list of product in the the its category. 
+    '''
+    @method Method to set list of product in the the its category.
+    @param
+    @return 
+    '''
     def set_gst_attr(self):
-                
-        
         print("Enter Numeric Code for product category \
             \nas per this \nfood-grains-701\nfurniture-702 \
             \nelectronics-703\ncosmetics-704"
@@ -56,14 +61,14 @@ class Gst:
                     print("You have entered a wrong code for your product category, Try again.....")
                     print("-----------------------------------------------------------------------")
                     __y = 1
-
-            
-
-            # how much item user want to enter for a product category.
+     '''
+    @description how much item user want to enter for a product category..
+    @param
+    @return
+    ''' 
             if __y == 0:
                 for _ in range(int(input(f"\nEnter number of item to be enter for \
                                 \n{self.__slab_codes[self.__slab_code]}:- "))):
-
                     if self.__slab_code==701:
                         slab_items =self.__set_gst_attri(0)
                         key = 'food-grains'
@@ -78,17 +83,15 @@ class Gst:
                         key = 'cosmetics'
                     else:
                         raise WrongValueError
-                    
-                    
-                    self.__gst_slab[key].append(slab_items)
-                
-                    
+                    self.__gst_slab[key].append(slab_items)    
                 self._x = "q!" if input("q/n to procced :-") == ("q" or "q!" ) else self._x
-
-    # Method to store the the attribute of product in list.
+    '''
+    @method  Method to store the the attribute of product in list.
+    @param
+    @return 
+    '''
     def __set_gst_attri(self,gst_percent):
                 slab_items = []
-                
                 if not slab_items:    
                     try:    
                         slab_items.append(int(input("\nEnter product unit :- ")))
@@ -108,24 +111,29 @@ class Gst:
                     slab_items.append(gst_percent)
                 return slab_items
                 
-                
-    # To output the calculated result of all the product with respective input.
+    '''
+    @method   To output the calculated result of all the product with respective input.
+    @param
+    @return
+    '''          
     def get_gst_price(self):
             print("{0:5s} {1:15s} {2:20s} {3:28s} {4:15s}".format("Unit","Commodity","Initial Unit Price","Applicable GST","Final Price"))   
             for key in self.__gst_slab:
                 if not (self.__gst_slab[key]):
                     continue
                 else:
-                
                     for final_price,i in self.gst_total_price(key):
-                    
                         print("%2d %8s %20.2f %23d %26.2f"%(self.__gst_slab[key][i][0],
                             self.__gst_slab[key][i][1],
                             self.__gst_slab[key][i][2],
                             self.__gst_slab[key][i][3],
                             final_price))    
 
-    # Method for calculating final price
+    '''
+    @method   Method for calculating final price.
+    @param
+    @return
+    '''          
     def gst_total_price(self,key):
         for i in range(len(self.__gst_slab[key])):
             final_value =  self.__gst_slab[key][i][0] * self.__gst_slab[key][i][2] + \
